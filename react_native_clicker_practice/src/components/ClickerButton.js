9707d7b4f95522c2283ada7c170c7b5552c84970
Synclair Wang <synclairwang11@yahoo.com>
2018-08-11 19:27:48 -0400

    ugh... animations

--- react_native_clicker_practice/src/components/ClickerButton.js
+++ react_native_clicker_practice/src/components/ClickerButton.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Animated } from 'react-native'
 
 class Button extends Component {
 
+    constructor(props) {
+        super(props)
+        this.animatedValue = new Animated.Value(0)
+    }
+
+    state = {
+        buttonScale: new Animated.Value(1)
+    }
+
     handlePress = () => {
+        Animated.timing(this.animatedValue, {
+            toValue: 1,
+            duration: 500
+        }).start()
         this.props.addPoint()
     }
 
     render() {
+
+        let { buttonScale} = this.state
+
         return (  
             <View style={styles.buttonContainerStyles}>
                 <TouchableOpacity onPress={this.handlePress} style={{ flex: 1 }}>
-                    <View style={styles.buttonStyles}>
+                    <Animated.View 
+                        style={
+                            [
+                                styles.buttonStyles,
+                                {
+                                    transform: [
+                                        {
+                                            scaleX: this.animatedValue.interpolate({
+                                                inputRange: [0, 1, 2],
+                                                outputRange: [1, .5, 1]
+                                            })
+                                        }, {
+                                            scaleY: this.animatedValue.interpolate({
+                                                inputRange: [0, 1, 2],
+                                                outputRange: [1, .5, 1]
+                                            })
+                                        }
+                                    ]
+                                }
+                            
+                            ]
+                        }
+                    >
                         <Text style={styles.buttonTextStyles}>CLICK ME</Text>
-                    </View>
+                    </Animated.View>
                 </TouchableOpacity>
             </View>
         );
@@ -29,7 +67,7 @@ const styles = {
         borderWidth: 2,
         borderColor: '#222',
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     buttonTextStyles: {
         alignSelf: 'center',
